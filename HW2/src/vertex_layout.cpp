237402648b3a356270caa71e201e165d5b476d71
Hyojeong Yu <hjeong.you@gmail.com>
2023-03-26 20:06:24 +0900

    clone imgui example

--- HW2/src/vertex_layout.cpp
+++ HW2/src/vertex_layout.cpp
@@ -21,7 +21,8 @@ void VertexLayout::SetAttrib(
     uint32_t type, bool normalized,
     size_t stride, uint64_t offset) const {
     glEnableVertexAttribArray(attribIndex);
-    glVertexAttribPointer(attribIndex, count, type, normalized, stride, (const void*)offset);
+    glVertexAttribPointer(attribIndex, count,
+        type, normalized, stride, (const void*)offset);
 }
 
 void VertexLayout::Init() {
