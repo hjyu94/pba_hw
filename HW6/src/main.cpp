d4529209f56913a5cd7ce7e458c3fc07638f0e02
Hyojeong Yu <hjeong.you@gmail.com>
2023-05-25 10:31:11 +0900

    branch upon build type

--- HW6/src/main.cpp
+++ HW6/src/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, const char** argv)
 
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HW5: Rigid Body Dynamics: Part 1 Colliding Contact", nullptr, nullptr);
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HW6: Rigid Body Dynamics Part 2: Resting Contact", nullptr, nullptr);
     if (!window) {
         SPDLOG_ERROR("failed to create glfw window");
         glfwTerminate();
